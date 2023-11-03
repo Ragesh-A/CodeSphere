2759e4f7f6ea91a1ce5c5b25dca0df8a049d9155
Ragesh-A <ragesha2017@gmail.com>
2023-11-03 10:18:14 +0530

    Angular: note: jit, aot

--- angular/first-app/src/app/app.component.ts
+++ angular/first-app/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // INTERPOLATION
+
+  // data binding we can access the value in html
+  // to access the value we use {{ variable name }}
   title = 'first-app';
+
+  // PROPERTY BINDING
+  
+  //other way to access dom attribute and modify the value
+  //<img [src]="imageUrl">
+  imageUrl = 'path to image url'
+  // <p [class.dark]='isDark'>
+  isDark = true || false;
+
+  // EVENT BINDING
+
+  // TO BIND EVENT <button (click)="clickHandle()">
+
 }
--- angular/first-app/src/main.ts
+++ angular/first-app/src/main.ts
@@ -1,4 +1,25 @@
+/* There are two type of compilation used by angular
+
+1. JIT (Just In Time)
+  - slower complier
+  - larger file
+  = needs to compile on server
+  - error on browser
+
+*/
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+/*
+
+2. AOT (Ahead Of Time)
+  - faster compile
+  - smaller size
+  - compile on delivery to browser
+  - error on server
+*/
+
+// import { platformBrowser } from '@angular/platform-browser';
+
 
 import { AppModule } from './app/app.module';
 
