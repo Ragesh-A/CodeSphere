c3f09e3852e10f244e57109b79a8f0b627fae96e
Ragesh-A <ragesha2017@gmail.com>
2023-11-03 10:37:49 +0530

    Angular: production mode

--- angular/first-app/src/main.ts
+++ angular/first-app/src/main.ts
@@ -20,6 +20,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 // import { platformBrowser } from '@angular/platform-browser';
 
+/*
+By default angular run app twice to check the the app work perfectly
+when the ui is not similar in both time it seems there is an error.
+This is helpful when in the development mode to figure it out the potential error.
+But we don't need this in the production mode enableProdMode help to convert
+app into production mode
+*/
+
+// import { enableProdMode } from '@angular/core';
+
+// check whether the app is production or development
+// if true call the function enableProdMode
+
 
 import { AppModule } from './app/app.module';
 
